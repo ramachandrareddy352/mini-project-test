91b377cf189e795b60f608152ae3c2853f62aa76
prasad <prasadpadala2005@gmail.com>
2025-01-26 15:41:14 +0530

    adding solana qr

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,78 @@
-export async function GET(request: Request) {
-  return new Response('Hello, from API!')
+import { clusterApiUrl, PublicKey,Connection, TransactionMessage, VersionedTransaction, SystemProgram, Transaction } from "@solana/web3.js";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+
+type GetData = {
+  label: string;
+  icon: string;
+};
+// Devnet 'fake' USDC, you can get these tokens from https://spl-token-faucet.com/
+const USDC_ADDRESS = new PublicKey("Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr");
+const ENDPOINT = clusterApiUrl("devnet");
+const NFT_NAME = "Golden Ticket";
+const PRICE_USDC = 0.1;
+type InputData = {
+  account: string;
+};
+type Data = {
+  label?: string;
+  icon?: string;
+  transaction?: string;
+  message?: string;
+};
+export type PostError = {
+  error: string;
+};
+
+
+
+
+export async function GET(
+  request: NextApiRequest,
+  response: NextApiResponse<Data>
+) {
+  const label = "Exiled Apes Academy";
+  const icon = "https://exiledapes.academy/wp-content/uploads/2021/09/X_share.png";
+
+  return NextResponse.json({label,icon},{status:200});
+}
+export async function POST(
+  request: NextApiRequest,
+  response: NextApiResponse<Data>
+) {
+  const accountField = request.body?.account;
+  if (!accountField) throw new Error('missing account');
+
+  const sender = new PublicKey(accountField);
+
+  // // create spl transfer instruction
+  // const splTransferIx = await createSplTransferIx(sender, connection);
+
+  // // create the transaction
+  // const transaction = new VersionedTransaction(
+  //   new TransactionMessage({
+  //       payerKey: sender,
+  //       recentBlockhash: '11111111111111111111111111111111',
+  //       // add the instruction to the transaction
+  //       instructions: [splTransferIx]
+  //   }).compileToV0Message()
+  // )
+
+  // Build Transction
+  const ix = SystemProgram.transfer({
+    fromPubkey:sender,
+    toPubkey:new PublicKey("E6LRKxEMkp3HGcSZBFroobrt5xDgssXNFNfJUe7i5KoR"),
+    lamports:1337000000 
+  });
+
+
+  const transaction = new Transaction();
+  transaction.add(ix);
+
+
+  const serializedTransaction = transaction.serialize();
+  const base64Transaction = Buffer.from(serializedTransaction).toString('base64');
+  const message = 'Thank you for your purchase of ExiledApe #518';
+
+  return NextResponse.json({ transaction: base64Transaction, message },{status:200});
 }
--- src/components/dashboard/dashboard-feature.tsx
+++ src/components/dashboard/dashboard-feature.tsx
@@ -1,31 +1,101 @@
-'use client'
+'use client';
 
-import { AppHero } from '../ui/ui-layout'
+import { useEffect, useRef, useState } from 'react';
+import { createQR, encodeURL, findReference, FindReferenceError } from '@solana/pay';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { ConfirmedSignatureInfo, Keypair, PublicKey, TransactionSignature } from '@solana/web3.js';
+import BigNumber from 'bignumber.js';
+import { simulateCheckout } from './simulateCheckout';
+import { validateTransfer } from './validate';
 
-const links: { label: string; href: string }[] = [
-  { label: 'Solana Docs', href: 'https://docs.solana.com/' },
-  { label: 'Solana Faucet', href: 'https://faucet.solana.com/' },
-  { label: 'Solana Cookbook', href: 'https://solanacookbook.com/' },
-  { label: 'Solana Stack Overflow', href: 'https://solana.stackexchange.com/' },
-  { label: 'Solana Developers GitHub', href: 'https://github.com/solana-developers/' },
-]
+const recipient = new PublicKey("7UhsoPTm5oYq3eubg4RpYgr2xAVY7L9RxLbSNhufg9yh");
+
 
 export default function DashboardFeature() {
+
+  const { connection } = useConnection();
+  const qrRef = useRef<HTMLDivElement>(null);
+  const [paymentStatus, setPaymentStatus] = useState('');
+  const [showQR, setShowQR] = useState(false);
+
+
+  const startPaymentTransfer = async () => {
+    setShowQR(true);
+    console.log('\n2. 🛍 Simulate a customer checkout \n');
+    const { label, message, memo, amount, reference } = await simulateCheckout();
+    // const reference = new Keypair().publicKey;
+    // const url = encodeURL({ recipient, amount, reference, label, message, memo });
+    console.log('3. 💰 Create a payment request link \n');
+    const url = encodeURL({ recipient: recipient, amount, reference, label, message, memo });
+    const SOLANA_PAY_URL = "solana:192.168.229.255:3000/api/hello"
+    const qr = createQR(SOLANA_PAY_URL, 360, 'white', 'black');
+    if (qrRef.current) {
+      qrRef.current.innerHTML = ''
+      qr.append(qrRef.current)
+    }
+    setPaymentStatus("Pending....")
+
+    console.log('\n5. Find the transaction');
+
+    const signatureInfo = await new Promise<ConfirmedSignatureInfo>((resolve, reject) => {
+      const interval = setInterval(async () => {
+        console.count('Checking for transaction...');
+        try {
+          const result = await findReference(connection, reference, { finality: 'confirmed' });
+          console.log(result);
+          console.log('\n 🖌  Signature found: ', result.signature);
+          clearInterval(interval);
+          resolve(result);
+        } catch (error: any) {
+          if (!(error instanceof FindReferenceError)) {
+            // console.error(error);
+            clearInterval(interval);
+            reject(error);
+          }
+        }
+      }, 2000);
+       // Add a timeout of 5 minutes
+      const timeout = setTimeout(() => {
+        clearInterval(interval);
+        console.log('❌ Payment timeout reached.');
+        setPaymentStatus("Timeout Reached");
+        reject(new Error('Payment timeout reached'));
+      }, 2 * 60 * 1000); // 5 minutes in milliseconds
+    });
+    const { signature } = signatureInfo;
+    setPaymentStatus("Confirmed");
+
+    console.log('\n6. 🔗 Validate transaction \n');
+
+    try {
+      await validateTransfer(connection, signature, { recipient: recipient, amount });
+
+      // Update payment status
+      setPaymentStatus('validated');
+      console.log('✅ Payment validated');
+      console.log('📦 Ship order to customer');
+    } catch (error) {
+      // console.error(error.message);
+      setPaymentStatus('Failed to Pay');
+    } finally {
+      setShowQR(false);
+    }
+  }
+
   return (
     <div>
-      <AppHero title="gm" subtitle="Say hi to your new Solana dApp." />
-      <div className="max-w-xl mx-auto py-6 sm:px-6 lg:px-8 text-center">
-        <div className="space-y-2">
-          <p>Here are some helpful links to get you started.</p>
-          {links.map((link, index) => (
-            <div key={index}>
-              <a href={link.href} className="link" target="_blank" rel="noopener noreferrer">
-                {link.label}
-              </a>
-            </div>
-          ))}
-        </div>
+      <div className="flex flex-col gap-8">
+        <button onClick={startPaymentTransfer}>StartPayment</button>
+        {
+          showQR &&
+          <>
+            <h1 className="text-3xl">Scan QR Code to Pay</h1>
+            <div ref={qrRef} />
+            <p>Status: <strong>{paymentStatus}</strong></p>
+          </>
+        }
+
       </div>
     </div>
-  )
+  );
 }
